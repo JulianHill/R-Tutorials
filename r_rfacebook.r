776da1d8394d6ff1ded0b5de8a31d83d74ec05a6
Julian Hillebrand <jul.hillebrand@gmail.com>
2017-01-16 22:00:42 +0100

    api changes

--- r_rfacebook.r
+++ r_rfacebook.r
@@ -1,18 +1,39 @@
-fbOAuth(app_id, app_secret, extended_permissions = TRUE)
+install.packages("devtools")
+library(devtools)
+
+install_github("Rfacebook", "pablobarbera", subdir="Rfacebook")
+
 
 require("Rfacebook")
-fb_oauth <- fbOAuth(app_id="123456789", app_secret="1A2B3C4D")
+
+
+
+
+
+######Using the App Authentication:
+
+fb_oauth <- fbOAuth(app_id="123456789", app_secret="1A2B3C4D",extended_permissions = TRUE)
+
 
 #now we have our fb_oauth connection
  #we will just save them to be able to use them later
- save(fb_oauth, file="fb_oauth“)
+ save(fb_oauth, file="fb_oauth")
  
  #so if you want to connect to Facebook again you just have to call
  load("fb_oauth")
 
+ me <- getUsers("me",token=fb_oauth)
+ 
+ my_likes <- getLikes(user="me", token=fb_oauth)
+ 
+ 
+ ######Using the Token Authentication:
 
-#the getUsers function return public information about one or more Facebook user
-me <- getUsers("me", token=fb_oauth)
+token <- 'YOUR AUTHENTICATION TOKEN'
+me <- getUsers("me", token, private_info=TRUE)
+
+getUsers(c("barackobama", "donaldtrump"), token)
+
 
 getFriends(token, simplify = FALSE)
 
